0c8d0140976f1be8dab36f3225eee046c2e2fd37
변대웅 <dwbyun16@gmail.com>
2022-02-26 23:23:54 +0900

    렌주룰 적용

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ let spaces = Array.from(new Array(19), (_, r) =>
 );
 const kanSize = 40;
 let turn = 1;
+const dr = [-1, -1, -1, 0, 1, 1, 1, 0];
+const dc = [-1, 0, 1, 1, 1, 0, -1, -1];
 
 function getRC(element, top, left) {
   const kanR = +element.id.split("c")[0].slice(1);
@@ -82,6 +84,13 @@ function addMouseClickEvent() {
       const { r, c } = getRC(e.target, top, left);
       if (spaces[r][c] > 0) return;
       spaces[r][c] = turn;
+
+      const ruleCheck = checkRules(r, c, turn);
+      if (!ruleCheck) {
+        spaces[r][c] = 0;
+        return;
+      }
+
       const al = document.createElement("div");
       al.className = `real-al real-al-${
         turn === 1 ? "black" : "white"
@@ -108,10 +117,7 @@ function checkGameEnd(r, c, turn) {
     }
     return curCount;
   }
-  const dr = [-1, -1, -1, 0, 1, 1, 1, 0];
-  const dc = [-1, 0, 1, 1, 1, 0, -1, -1];
   const counts = dr.map((_, i) => count(r, c, dr[i], dc[i], turn, 0));
-  console.log(counts);
   for (let i = 0; i < 4; i++) {
     if (counts[i] + counts[i + 4] === 4) {
       setTimeout(() => {
@@ -121,6 +127,77 @@ function checkGameEnd(r, c, turn) {
       break;
     }
   }
+}
+
+function checkRules(r, c, turn) {
+  if (turn === 2) return true;
+  function count(r, c, dr, dc, turn, curCount, blankCount) {
+    const nr = r + dr;
+    const nc = c + dc;
+    if (nr >= 0 && nr < spaces.length && nc >= 0 && nc < spaces[0].length) {
+      if (spaces[nr][nc] === turn)
+        return count(nr, nc, dr, dc, turn, curCount + 1, blankCount);
+      else if (spaces[nr][nc] === 0) {
+        if (blankCount === 0)
+          return count(nr, nc, dr, dc, turn, curCount, blankCount + 1);
+        else return { count: curCount, isBlocked: false };
+      } else {
+        if (spaces[r][c] === 0) {
+          return {
+            count: curCount,
+            isBlocked: false,
+          };
+        } else {
+          return {
+            count: curCount,
+            isBlocked: true,
+          };
+        }
+      }
+    }
+    return { count: curCount, isBlocked: true };
+  }
+  const counts = dr.map((_, i) => count(r, c, dr[i], dc[i], turn, 0, 0));
+  const countLine = [{}, {}, {}, {}];
+  for (let i = 0; i < 4; i++) {
+    countLine[i] = {
+      count: counts[i].count + counts[i + 4].count,
+      blockedCount: counts[i].isBlocked
+        ? 1
+        : 0 + counts[i + 4].isBlocked
+        ? 1
+        : 0,
+    };
+  }
+  // 33 체크
+  const count3 = countLine.reduce(
+    (prev, cur) =>
+      cur.count === 2 && cur.blockedCount === 0 ? prev + 1 : prev,
+    0
+  );
+  if (count3 > 1) {
+    alert("흑은 3 3을 놓을 수 없습니다.");
+    return false;
+  }
+  // 44 체크
+  const count4 = countLine.reduce(
+    (prev, cur) => (cur.count === 3 && cur.blockedCount < 2 ? prev + 1 : prev),
+    0
+  );
+  if (count4 > 1) {
+    alert("흑은 4 4를 놓을 수 없습니다.");
+    return false;
+  }
+  // 6목 체크
+  const count6 = countLine.reduce(
+    (prev, cur) => (cur.count === 5 ? prev + 1 : prev),
+    0
+  );
+  if (count6 > 0) {
+    alert("흑은 6목을 놓을 수 없습니다.");
+    return false;
+  }
+  return true;
 }
 
 initPan();
