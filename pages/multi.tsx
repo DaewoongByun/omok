91eaca572b4ec3ad5ab5f373bb31ab84947ee90b
변대웅 <dwbyun16@gmail.com>
2022-03-29 16:41:53 +0900

    버튼 추가

--- pages/multi.tsx
+++ pages/multi.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import OmokPan from "../components/OmokPan";
 import { checkGameEnd, checkRules } from "../utils/check";
+import { useRouter } from "next/router";
 
 export default function Multi() {
   const [turn, setTurn] = useState<number>(1);
   const [pan, setPan] = useState<Array<Array<number>>>(
     Array.from(new Array(19), () => Array.from(new Array(19), () => 0))
   );
+  const router = useRouter();
 
   function init() {
     setTurn(1);
@@ -24,7 +26,8 @@ export default function Multi() {
     const endCheckResult = checkGameEnd(pan, r, c, turn);
 
     if (endCheckResult) {
-      alert(endCheckResult);
+      const result = alert(endCheckResult);
+      console.log(result);
       init();
       return;
     }
@@ -34,14 +37,28 @@ export default function Multi() {
 
   return (
     <>
-      <div>
+      <div className="container">
+        <div className="buttons">
+          <button onClick={init}>새 게임</button>
+          <button onClick={() => router.back()}>뒤로가기</button>
+        </div>
         <OmokPan pan={pan} turn={turn} onClick={handleClick} />
       </div>
       <style jsx>
         {`
-          div {
+          .container {
+            width: 100%;
+            padding-top: 20px;
             display: flex;
+            flex-direction: column;
+            gap: 20px;
+            align-items: center;
             justify-content: center;
+          }
+          .buttons {
+            display: flex;
+            flex-direction: row;
+            gap: 20px;
           }
         `}
       </style>
